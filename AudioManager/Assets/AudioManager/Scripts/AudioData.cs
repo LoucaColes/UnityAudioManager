79d556cd9a20629cd9f49d5df2bc9b9c284190bb
LoucaColes <coleslouca@yahoo.co.uk>
2017-11-02 11:23:41 +0000

    Changes Made - Music will stop if the next track has a fade in value of 0 - Music and SFX can be played using an id value instead of a name - Audio Manager can have multiple background music enteries for dynamic music - Audio Manager has its own namespace - Audio Manager and Audio Data are now two different c-sharp scripts - Test input script to help test systems work

---
+++ AudioManager/Assets/AudioManager/Scripts/AudioData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace AudioManager
+{
+    [System.Serializable]
+    public class AudioData
+    {
+        public string m_name;
+        public AudioClip m_audioClip;
+
+        public bool m_looping;
+
+        [Range(0f, 1f)]
+        public float m_volume;
+
+        [Range(0.3f, 3f)]
+        public float m_pitch;
+
+        [Range(0f, 5f)]
+        public float m_fadeInTime;
+
+        [Range(0f, 5f)]
+        public float m_fadeOutTime;
+
+        [HideInInspector]
+        public GameObject m_object;
+
+        [HideInInspector]
+        public AudioSource m_audioSource;
+    }
+}
