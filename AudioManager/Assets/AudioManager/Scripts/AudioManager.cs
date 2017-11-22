d6192b4cbddf678ba3f75c9497387e731056aefc
LoucaColes <coleslouca@yahoo.co.uk>
2017-11-22 11:24:37 +0000

    Finished Jukebox and commented through jukebox script and just under half of the audio manager.

--- AudioManager/Assets/AudioManager/Scripts/AudioManager.cs
+++ AudioManager/Assets/AudioManager/Scripts/AudioManager.cs
@@ -5,103 +5,142 @@ using UnityEngine;
 
 namespace AudioManager
 {
+    // Main class that controls the audio
     public class AudioManager : MonoBehaviour
     {
+        // Singleton of the audio manager
         public static AudioManager m_instance;
 
+        // List of empty game objects used to store the
+        // different types of audio
         [SerializeField]
         private List<GameObject> m_categories;
 
+        // Array of audio data for background music
         public AudioData[] m_backgroundMusicList;
 
+        // Array of audio data for sfx
         public AudioData[] m_sfxList;
 
+        // Array of audio data for voice
         public AudioData[] m_voiceList;
 
+        // Current background music
         [SerializeField]
         private AudioData m_currentMusic;
 
+        // Previous background music
         [SerializeField]
         private AudioData m_prevMusic;
 
+        // List of the current sfx playing
         [SerializeField]
         private List<AudioData> m_currentSFX;
 
+        // List of the current voice lines playing
         [SerializeField]
         private List<AudioData> m_currentVoices;
 
+        // Global sfx volume
         [Range(0, 1), SerializeField]
         private float m_sfxGlobalVolume = 1f;
 
+        // Global music volume
         [Range(0, 1), SerializeField]
         private float m_musicGlobalVolume = 1f;
 
+        // Global voice volume
         [Range(0, 1), SerializeField]
         private float m_voiceGlobalVolume = 1f;
 
+        // Global master volume
         [Range(0, 1), SerializeField]
         private float m_masterVolume = 1f;
 
+        // Minimum and maximum volume for random volume
         [SerializeField]
         private Vector2 m_volumeMinMax;
 
+        // Minimum and maximum pitch for random pitch
         [SerializeField]
         private Vector2 m_pitchMinMax;
 
         private void Awake()
         {
+            // Create the various lists
             m_categories = new List<GameObject>();
             m_currentSFX = new List<AudioData>();
             m_currentVoices = new List<AudioData>();
 
+            // Set the object to don't destroy on load
             DontDestroyOnLoad(gameObject);
 
+            // Create singleton of the Audio Manager
             CreateInstance();
 
+            // Set up the audio arrays
             SetUpAudioArray(m_backgroundMusicList, "Background Music");
 
             SetUpAudioArray(m_sfxList, "SFX");
 
             SetUpAudioArray(m_voiceList, "Voice Lines");
 
+            // Clear the current and previous music variables
             ClearCurrentPrevMusic();
         }
 
+        // Creates singleton of the jukebox
         private void CreateInstance()
         {
+            // If there is no instance set this
+            // as the instance
             if (!m_instance)
             {
                 m_instance = this;
             }
+            // Else destroy the object with this
+            // script
             else
             {
                 Destroy(gameObject);
             }
         }
 
+        // Set up the audio array
         private void SetUpAudioArray(AudioData[] _array, string _arrayName)
         {
+            // Create an empty game object for the different types of sound
             GameObject t_catergory = new GameObject(_arrayName);
-            t_catergory.transform.parent = transform;
-            m_categories.Add(t_catergory);
+            t_catergory.transform.parent = transform; // Parent to audio manager
+            m_categories.Add(t_catergory); // Add to list of catergories
 
+            // For each array entry create empty game object
             for (int i = 0; i < _array.Length; i++)
             {
+                // Name the object based on the sounds name
                 GameObject t_child = new GameObject(_array[i].m_name);
+                // Parent to the catergory game object
                 t_child.transform.parent = t_catergory.transform;
 
+                // Create a audio source for the sound object
                 AudioSource t_audioSource = t_child.AddComponent<AudioSource>();
 
+                // Transfer game object and audio source to the track's audio data
                 _array[i].m_object = t_child;
                 _array[i].m_audioSource = t_audioSource;
 
+                // Transfer track's audio data's audio clip to the audio source
                 _array[i].m_audioSource.clip = _array[i].m_audioClip;
+
+                // Update the rest of the audio source data with the audio data's version
                 UpdateData(_array, i);
             }
         }
 
+        // Update an arrays volume
         private void UpdateArrayVolume(AudioData[] _array)
         {
+            // Loop through each entry and update the volume based on the global volumes
             for (int i = 0; i < _array.Length; i++)
             {
                 _array[i].m_volume = UpdateAudioDataVolume(_array[i]);
@@ -109,118 +148,159 @@ namespace AudioManager
             }
         }
 
+        // Update volumes based on global volumes
         private float UpdateAudioDataVolume(AudioData _audioData)
         {
+            // If the audio type is music then use the global music volume
             if (_audioData.m_type == AudioData.AudioType.MUSIC)
             {
                 return _audioData.GetOriginalVolume() * m_musicGlobalVolume * m_masterVolume;
             }
+            // If the audio type is sfx then use the global sfx volume
             else if (_audioData.m_type == AudioData.AudioType.SFX)
             {
                 return _audioData.GetOriginalVolume() * m_sfxGlobalVolume * m_masterVolume;
             }
+            // Else presume the audio type is volume (only 3 types at the moment)
+            // and use the global voice volume
             else
             {
                 return _audioData.GetOriginalVolume() * m_voiceGlobalVolume * m_masterVolume;
             }
         }
 
+        // Set the current and previous music track to null
         private void ClearCurrentPrevMusic()
         {
             m_currentMusic = null;
             m_prevMusic = m_currentMusic;
         }
 
+        // Play music by passing in the name of the track
         public void PlayMusic(string _name)
         {
             Debug.Log("Playing Music");
+            // Check if there is an audio data based on the passed in name
             AudioData t_data = Array.Find(m_backgroundMusicList, bgm => bgm.m_name == _name);
+            // If there isn't display error and return
             if (t_data == null)
             {
                 Debug.LogError("Didnt find music");
                 return;
             }
+            // Else play new track
             else
             {
+                // Set previous music to the current
+                // Set current to the found audio data
                 m_prevMusic = m_currentMusic;
                 m_currentMusic = t_data;
-                PlayNextMusicTrack();
+                PlayNextMusicTrack(); // Play new track
             }
         }
 
+        // Play music by passing in the name of the track and a position in world space
         public void PlayMusic(string _name, Vector3 _pos)
         {
             Debug.Log("Playing Music");
+            // Check if there is an audio data based on the passed in name
             AudioData t_data = Array.Find(m_backgroundMusicList, bgm => bgm.m_name == _name);
+            // If there isn't display error and return
             if (t_data == null)
             {
                 Debug.LogError("Didnt find music");
                 return;
             }
+            // Else play new track
             else
             {
+                // Set previous music to the current
+                // Set current to the found audio data
                 m_prevMusic = m_currentMusic;
                 m_currentMusic = t_data;
+                // Set found audio data's object position
                 t_data.m_object.transform.position = _pos;
-                PlayNextMusicTrack();
+                PlayNextMusicTrack(); // Play new track
             }
         }
 
+        // Play music by passing in the id of the track
         public void PlayMusic(int _id)
         {
+            // Check if the passed in id is valid
             if (_id >= 0 && _id < m_backgroundMusicList.Length)
             {
                 Debug.Log("Playing Music");
 
+                // Set previous music to current music
                 m_prevMusic = m_currentMusic;
+                // Set current music to new track
                 m_currentMusic = m_backgroundMusicList[_id];
+                // Play new track
                 PlayNextMusicTrack();
             }
-            else
+            else // If not valid
             {
+                // Display debug message and return
                 Debug.LogError("Didnt find music");
                 return;
             }
         }
 
+        // Play music by passing in the name of the track and a position in world space
         public void PlayMusic(int _id, Vector3 _pos)
         {
+            // Check if the passed in id is valid
             if (_id >= 0 && _id < m_backgroundMusicList.Length)
             {
                 Debug.Log("Playing Music");
 
+                // Set previous music to current music
                 m_prevMusic = m_currentMusic;
+                // Set current music to new track
                 m_currentMusic = m_backgroundMusicList[_id];
+                // Set the current music's audio data's object position
                 m_currentMusic.m_object.transform.position = _pos;
+                // Play new track
                 PlayNextMusicTrack();
             }
-            else
+            else // If not valid
             {
+                // Display debug message and return
                 Debug.LogError("Didnt find music");
                 return;
             }
         }
 
+        // Play the new track
         private void PlayNextMusicTrack()
         {
+            // Check if there is a delay
             if (m_currentMusic.m_delayTime > 0 || m_currentMusic.m_randDelay)
             {
+                // Set the delay time
                 float t_delayTime = m_currentMusic.m_delayTime;
                 if (m_currentMusic.m_randDelay)
                 {
                     t_delayTime = UnityEngine.Random.Range(0.1f, 1f);
                 }
+                // Play track after delayed time
                 m_currentMusic.m_audioSource.PlayDelayed(t_delayTime);
             }
-            else
+            else // If no delay
             {
+                // Play the new track
                 m_currentMusic.m_audioSource.Play();
+                // If there is no fade set
                 if (!m_currentMusic.m_fade && m_prevMusic != null)
                 {
+                    // Stop the previous music track
                     m_prevMusic.m_audioSource.Stop();
                 }
+                // If there is fade set
                 if (m_currentMusic.m_fade)
                 {
+                    // Fade both music tracks
                     StartCoroutine(FadeIn(m_currentMusic));
                     if (m_prevMusic != null)
                     {
@@ -230,59 +310,72 @@ namespace AudioManager
             }
         }
 
+        // Play sfx by passing in the sounds name
         public void PlaySFX(string _name)
         {
+            // Check if that sound exists
             AudioData t_data = Array.Find(m_sfxList, sfx => sfx.m_name == _name);
-            if (t_data == null)
+            if (t_data == null) // If it doesn't display error and return
             {
                 Debug.LogError("Didnt find sound");
                 return;
             }
-            else
+            else // If it exists play sfx
             {
                 PlayFoundSFX(t_data);
             }
         }
 
+        // Play sfx by passing in the name and a world position
+        public void PlaySFX(string _name, Vector3 _pos)
+        {
+            // Check if that sfx exists
+            AudioData t_data = Array.Find(m_sfxList, sfx => sfx.m_name == _name);
+            if (t_data == null) // If it doesn't exist display error message and return
+            {
+                Debug.LogError("Didnt find sound");
+                return;
+            }
+            else // If it exists then play the sfx
+            {
+                t_data.m_object.transform.position = _pos;
+                PlayFoundSFX(t_data);
+            }
+        }
+
+        // Play the found sfx audio data
         private void PlayFoundSFX(AudioData t_data)
         {
+            // Check if the user wants a random volume and if true then randomise the volume
             if (t_data.m_randVolume)
             {
-                t_data.m_audioSource.volume = UnityEngine.Random.Range(0.1f, 1);
+                t_data.m_audioSource.volume = UnityEngine.Random.Range(m_volumeMinMax.x, m_volumeMinMax.y);
             }
+            // Check if the user wants a random pitch and if true then randomise the pitch
             if (t_data.m_randPitch)
             {
-                t_data.m_audioSource.pitch = UnityEngine.Random.Range(0.3f, 3);
+                t_data.m_audioSource.pitch = UnityEngine.Random.Range(m_pitchMinMax.x, m_pitchMinMax.y);
             }
+            // Check if there is a delay
+            // If there isn't then just play the sfx
             if (t_data.m_delayTime == 0)
             {
                 t_data.m_audioSource.Play();
             }
+            // If there is
             else if (t_data.m_delayTime > 0 || t_data.m_randDelay)
             {
+                // Set the delay time
                 float t_delayTime = t_data.m_delayTime;
                 if (t_data.m_randDelay)
                 {
                     t_delayTime = UnityEngine.Random.Range(0.1f, 1f);
                 }
+                // Play the sfx after the delayed time
                 t_data.m_audioSource.PlayDelayed(t_delayTime);
             }
+            // Add the sfx to the currently playing sfx list
             m_currentSFX.Add(t_data);
-        }
-
-        public void PlaySFX(string _name, Vector3 _pos)
-        {
-            AudioData t_data = Array.Find(m_sfxList, sfx => sfx.m_name == _name);
-            if (t_data == null)
-            {
-                Debug.LogError("Didnt find sound");
-                return;
-            }
-            else
-            {
-                t_data.m_object.transform.position = _pos;
-                PlayFoundSFX(t_data);
-            }
         }
 
         public void PlaySFX(int _id)
@@ -511,7 +604,13 @@ namespace AudioManager
             {
                 m_musicGlobalVolume = _volume;
                 UpdateArrayVolume(m_backgroundMusicList);
+                Jukebox.m_instance.UpdateVolume();
             }
+        }
+
+        public float GetMusicGlobalVolume()
+        {
+            return m_musicGlobalVolume;
         }
 
         public void SetVoiceGlobalVolume(float _volume)
@@ -531,7 +630,13 @@ namespace AudioManager
                 UpdateArrayVolume(m_sfxList);
                 UpdateArrayVolume(m_backgroundMusicList);
                 UpdateArrayVolume(m_voiceList);
+                Jukebox.m_instance.UpdateVolume();
             }
+        }
+
+        public float GetMasterVolume()
+        {
+            return m_masterVolume;
         }
 
         public void UpdateGlobalVolumes(float _sfxVolume, float _musicVolume, float _masterVolume)
@@ -552,6 +657,7 @@ namespace AudioManager
             UpdateArrayVolume(m_sfxList);
             UpdateArrayVolume(m_backgroundMusicList);
             UpdateArrayVolume(m_voiceList);
+            Jukebox.m_instance.UpdateVolume();
         }
 
         public void UpdateSoundVariables(AudioData.AudioType _audioType, int _id, float _volume, float _pitch, bool _looping, float _panStereo, float _spatialBlend, int _priority, float _startDelay)
@@ -608,7 +714,7 @@ namespace AudioManager
             }
         }
 
-        private void UpdateData(AudioData[] _array, int _id)
+        public void UpdateData(AudioData[] _array, int _id)
         {
             _array[_id].SetOriginalVolume(_array[_id].m_volume);
             _array[_id].m_volume = UpdateAudioDataVolume(_array[_id]);
---
+++ AudioManager/Assets/AudioManager/Scripts/Jukebox.cs
@@ -0,0 +1,288 @@
+﻿using UnityEngine;
+
+namespace AudioManager
+{
+    // Jukebox class where it has a queue of music tracks
+    [System.Serializable]
+    public class Jukebox : MonoBehaviour
+    {
+        // Array of the tracks to play
+        public AudioData[] m_playlist;
+
+        // Id of the current track
+        [SerializeField]
+        private int m_id;
+
+        // Current track playing
+        [SerializeField]
+        private AudioData m_currentlyPlaying;
+
+        // Possible states of the jukebox
+        public enum JukeboxState
+        {
+            SetUp,
+            Play,
+            Pause,
+            Stop,
+        }
+
+        // Current state of the jukebox
+        private JukeboxState m_state;
+
+        // Types of play
+        public enum JukeboxPlayType
+        {
+            Random,
+            Sequential,
+        }
+
+        // Current play type of the jukebox
+        public JukeboxPlayType m_playType;
+
+        // Length of the current track
+        [SerializeField]
+        private float m_playTime;
+
+        // Timer to track the current point in the track
+        [SerializeField]
+        private float m_timer;
+
+        // Singleton of the Jukebox
+        public static Jukebox m_instance;
+
+        // Start Function
+        // Creates singleton of the jukebox
+        // Sets up the jukebox state
+        // Sets up the jukebox
+        private void Start()
+        {
+            CreateInstance();
+            m_state = JukeboxState.SetUp;
+            SetUp();
+        }
+
+        // Creates singleton of the jukebox
+        private void CreateInstance()
+        {
+            // If there is no instance set this
+            // as the instance
+            if (!m_instance)
+            {
+                m_instance = this;
+            }
+            // Else destroy the object with this
+            // script
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        // Automatic track changing
+        private void Update()
+        {
+            // If the jukebox is in the play state
+            if (m_state == JukeboxState.Play)
+            {
+                // Increase the timer
+                m_timer += Time.deltaTime;
+                if (m_timer >= m_playTime)
+                {
+                    // If the timer is greater or equal to the play time
+                    // of the track, play the next one
+                    switch (m_playType)
+                    {
+                        // If play type is sequential play the next one
+                        case JukeboxPlayType.Sequential:
+                            PlayNext();
+                            break;
+
+                        // If play type is random play a random track
+                        case JukeboxPlayType.Random:
+                            PlayRandom();
+                            break;
+                    }
+                }
+            }
+        }
+
+        // Set up the jukebox
+        private void SetUp()
+        {
+            // Set up the playlist
+            SetUpAudioArray(m_playlist, "Playlist");
+            m_id = 0; // Set id
+            // Set the current playing track
+            m_currentlyPlaying = m_playlist[m_id];
+
+            // Reset the timer
+            m_timer = 0;
+
+            // Set the play time of the current track
+            m_playTime = m_currentlyPlaying.m_audioClip.length;
+
+            // Play the current track
+            PlayCurrent();
+
+            // Set the state to play
+            m_state = JukeboxState.Play;
+        }
+
+        // Set up the playlist array (similar to the Audio Manager version)
+        private void SetUpAudioArray(AudioData[] _array, string _arrayName)
+        {
+            // Create a empty game object to store the track objects
+            GameObject t_catergory = new GameObject(_arrayName);
+            t_catergory.transform.parent = transform;
+
+            // Create a empty game object for each track
+            for (int i = 0; i < _array.Length; i++)
+            {
+                GameObject t_child = new GameObject(_array[i].m_name);
+                t_child.transform.parent = t_catergory.transform;
+
+                // Add an audio source to the empty game object
+                AudioSource t_audioSource = t_child.AddComponent<AudioSource>();
+
+                // Transfer game object and audio source to the track's audio data
+                _array[i].m_object = t_child;
+                _array[i].m_audioSource = t_audioSource;
+
+                // Transfer track's audio data's audio clip to the audio source
+                _array[i].m_audioSource.clip = _array[i].m_audioClip;
+
+                // Update the rest of the audio source data with the audio data's version
+                AudioManager.m_instance.UpdateData(_array, i);
+            }
+        }
+
+        // Pause/Unpause Jukebox
+        public void PauseJukebox()
+        {
+            // If the track is playing the pause
+            if (m_currentlyPlaying.m_audioSource.isPlaying)
+            {
+                // pause the current track
+                m_currentlyPlaying.m_audioSource.Pause();
+                m_state = JukeboxState.Pause; // Set the jukebox state to pause
+            }
+            // If the track isn't playing then unpause the track
+            else
+            {
+                // Update the volume just in case there was a volume change
+                UpdateVolume();
+                // Unpause the current track
+                m_currentlyPlaying.m_audioSource.UnPause();
+                // Set the jukebox state to play
+                m_state = JukeboxState.Play;
+            }
+        }
+
+        // Stop the jukebox
+        public void StopJukebox()
+        {
+            // Stop the current track
+            m_currentlyPlaying.m_audioSource.Stop();
+            // Set state to stop
+            m_state = JukeboxState.Stop;
+        }
+
+        // Play Jukebox if paused or stop
+        public void PlayJukebox()
+        {
+            switch (m_state)
+            {
+                // If the jukebox was in the stop state
+                case JukeboxState.Stop:
+                    m_timer = 0; // Reset timer
+                    // Set play time
+                    m_playTime = m_currentlyPlaying.m_audioClip.length;
+
+                    // Play the current track
+                    PlayCurrent();
+                    // Set the state to play
+                    m_state = JukeboxState.Play;
+                    break;
+
+                // If the jukebox was in the pause state
+                case JukeboxState.Pause:
+                    // Unpause the track
+                    PauseJukebox();
+                    break;
+            }
+        }
+
+        // Play Next Track
+        public void PlayNext()
+        {
+            // Stop the current track
+            m_currentlyPlaying.m_audioSource.Stop();
+
+            m_id++; // Increment the id value
+
+            // If the id reaches the limit reset to 0
+            if (m_id >= m_playlist.Length)
+            {
+                m_id = 0;
+            }
+
+            // Update the current track variable
+            m_currentlyPlaying = m_playlist[m_id];
+
+            // Reset timer
+            m_timer = 0;
+
+            // Update the playtime variable
+            m_playTime = m_currentlyPlaying.m_audioClip.length;
+
+            // Play the new track
+            PlayCurrent();
+        }
+
+        // Play Random Track
+        public void PlayRandom()
+        {
+            // Generate a random int until different to current id
+            int t_int = m_id;
+            do
+            {
+                t_int = Random.Range(0, m_playlist.Length);
+            } while (t_int == m_id);
+            m_id = t_int; // Set current id to new id
+
+            // Stop the current track
+            m_currentlyPlaying.m_audioSource.Stop();
+
+            // Set the new current track
+            m_currentlyPlaying = m_playlist[m_id];
+
+            // Reset timer
+            m_timer = 0;
+
+            // Set the play time
+            m_playTime = m_currentlyPlaying.m_audioClip.length;
+
+            // Play the new track
+            PlayCurrent();
+        }
+
+        // Play the current track
+        private void PlayCurrent()
+        {
+            // Update the volume in case there was a volume change
+            UpdateVolume();
+
+            // Play current track
+            m_currentlyPlaying.m_audioSource.Play();
+        }
+
+        // Update volume of current track
+        public void UpdateVolume()
+        {
+            // Calculate the new volume and set the volume of current track
+            m_currentlyPlaying.m_audioSource.volume = m_currentlyPlaying.GetOriginalVolume() *
+                AudioManager.m_instance.GetMusicGlobalVolume() *
+                AudioManager.m_instance.GetMasterVolume();
+        }
+    }
+}
